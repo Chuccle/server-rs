3e946c91433c11a81760478bcc615d217b784cc1
Charlie <charlie11cohen@gmail.com>
2025-02-07 15:46:05 +0000

    refactor and switch to using capn proto

--- src/utils/windows.rs
+++ src/utils/windows.rs
@@ -2,10 +2,10 @@ pub mod time {
 
     const WINDOWS_EPOCH_OFFSET: u64 = 11_644_473_600 * 10_000_000; // 1601 to 1970 in 100-ns intervals
 
-    #[derive(Debug, Clone, Copy, serde::Serialize, serde::Deserialize)]
-    pub struct FileTime(u64);
+    #[derive(Debug, Clone, Copy)]
+    pub struct File(u64);
 
-    impl From<std::time::SystemTime> for FileTime {
+    impl From<std::time::SystemTime> for File {
         fn from(st: std::time::SystemTime) -> Self {
             // Calculate duration since UNIX epoch
             let duration = st
@@ -14,16 +14,22 @@ pub mod time {
 
             // Convert to 100-nanosecond intervals
             let total_100ns = (duration.as_secs() * 10_000_000)
-                + (duration.subsec_nanos() as u64 / 100)
+                + (u64::from(duration.subsec_nanos()) / 100)
                 + WINDOWS_EPOCH_OFFSET;
 
-            FileTime(total_100ns)
+            Self(total_100ns)
         }
     }
 
-    impl From<FileTime> for u64 {
-        fn from(ft: FileTime) -> u64 {
+    impl From<File> for u64 {
+        fn from(ft: File) -> Self {
             ft.0
+        }
+    }
+
+    impl From<u64> for File {
+        fn from(num: u64) -> Self {
+            Self(num)
         }
     }
 }
